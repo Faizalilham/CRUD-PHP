1ec9dd552077456df7e383f2588195bbc46be277
Faizalilham <72005812+Faizalilham@users.noreply.github.com>
2022-03-11 16:39:47 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -7,7 +7,7 @@ $password = 'cce971decf7500cae633f5f5a8cab24677340cd75265c3546b42fc60a80cf3c1';
 $db   = 'd8pemi3akc8258';
 
 $connection = pg_connect("host=$host port =$port dbname=$db user=$user password=$password");
-if($koneksi){
+if($connection){
 echo 'Koneksi Berhasil';
 }
 else{
